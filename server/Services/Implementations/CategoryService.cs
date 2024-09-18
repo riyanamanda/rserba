56bfe76db2c3e229d4051601aac565297e5e22bd
Riyan Amanda <ryant.n92@gmail.com>
2024-09-18 13:57:35 +0700

    refactor: add return response on category

--- server/Services/Implementations/CategoryService.cs
+++ server/Services/Implementations/CategoryService.cs
@@ -10,7 +10,7 @@ namespace server.Services.Implementations;
 
 public class CategoryService : ICategoryService
 {
-    public async Task<DataResponse> GetAll(ICategoryRepository categoryRepository)
+    public async Task<object> GetAll(ICategoryRepository categoryRepository)
     {
         var categories = (await categoryRepository.GetAll()).Select(category => category.AsDto());
         return new DataResponse(HttpStatusCode.OK, "success", categories);
@@ -38,17 +38,17 @@ public class CategoryService : ICategoryService
         return new Response(HttpStatusCode.Created, "Category has been created successfully");
     }
 
-    public Task Delete(ICategoryRepository categoryRepository, string slug)
+    public Task<Response> FindBySlug(ICategoryRepository categoryRepository, string slug)
     {
         throw new NotImplementedException();
     }
 
-    public Task FindBySlug(ICategoryRepository categoryRepository, string slug)
+    public Task<Response> Update(ICategoryRepository categoryRepository, string slug, UpdateCategoryDto request)
     {
         throw new NotImplementedException();
     }
 
-    public Task Update(ICategoryRepository categoryRepository, string slug, UpdateCategoryDto request)
+    public Task<Response> Delete(ICategoryRepository categoryRepository, string slug)
     {
         throw new NotImplementedException();
     }
--- server/Services/Interfaces/ICategoryService.cs
+++ server/Services/Interfaces/ICategoryService.cs
@@ -5,9 +5,9 @@ namespace server.Services.Interfaces;
 
 public interface ICategoryService
 {
-    Task<DataResponse> GetAll(ICategoryRepository categoryRepository);
+    Task<object> GetAll(ICategoryRepository categoryRepository);
     Task<Response> Create(ICategoryRepository categoryRepository, CreateCategoryDto request);
-    Task FindBySlug(ICategoryRepository categoryRepository, string slug);
-    Task Update(ICategoryRepository categoryRepository, string slug, UpdateCategoryDto request);
-    Task Delete(ICategoryRepository categoryRepository, string slug);
+    Task<Response> FindBySlug(ICategoryRepository categoryRepository, string slug);
+    Task<Response> Update(ICategoryRepository categoryRepository, string slug, UpdateCategoryDto request);
+    Task<Response> Delete(ICategoryRepository categoryRepository, string slug);
 }
