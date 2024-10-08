f49986200a2f61129f1baa6c0cd200c3f76274ba
Riyan Amanda <ryant.n92@gmail.com>
2024-10-08 15:43:12 +0700

    feat: add doctor photo on hero section

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,15 +2,17 @@
 
 import { Features } from '@/components/feature';
 import AppLayout from '@/components/layout/AppLayout';
+import { HoverBorderGradient } from '@/components/ui/hover-border-gradient';
 import { Spotlight } from '@/components/ui/Spotlight';
 import { TextGenerateEffect } from '@/components/ui/text-generate-effect';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
     return (
         <AppLayout>
             {/* Hero */}
-            <section className='flex items-center justify-center h-full w-full flex-col space-y-3 py-20 mb-24'>
+            <section className='flex items-center justify-center h-full w-full flex-col space-y-3 py-20 mb-20'>
                 <Spotlight className='h-screen -left-20' />
 
                 <div className='flex justify-between w-full mb-56 h-96'>
@@ -26,28 +28,63 @@ export default function Home() {
                         />
                     </div>
 
-                    <div className='w-6/12 flex items-center h-fit gap-x-2'>
-                        <Image
-                            src='https://images.unsplash.com/photo-1651008376811-b90baee60c1f?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-                            alt='Dokter'
-                            width={200}
-                            height={100}
-                            className='w-auto h-full rounded-s-lg'
-                        />
-                        <Image
-                            src='https://images.unsplash.com/photo-1651008376811-b90baee60c1f?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-                            alt='Dokter'
-                            width={200}
-                            height={100}
-                            className='w-auto h-full'
-                        />
-                        <Image
-                            src='https://images.unsplash.com/photo-1651008376811-b90baee60c1f?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-                            alt='Dokter'
-                            width={200}
-                            height={100}
-                            className='w-auto h-full rounded-e-lg'
-                        />
+                    <div className='w-6/12 h-full'>
+                        <div className='grid grid-cols-3 gap-2'>
+                            <Image
+                                src='/images/dokter/dr-abdullah.png'
+                                alt='Dokter'
+                                className='w-80 rounded-s-lg h-full aspect-auto'
+                                width={500}
+                                height={500}
+                            />
+                            <Image
+                                src='/images/dokter/dr-dini.png'
+                                alt='Dokter'
+                                className='w-80 h-full aspect-auto'
+                                width={500}
+                                height={500}
+                            />
+                            <Image
+                                src='/images/dokter/dr-ferry.png'
+                                alt='Dokter'
+                                className='w-80 rounded-e-lg h-full aspect-auto'
+                                width={500}
+                                height={500}
+                            />
+                        </div>
+
+                        <blockquote className='relative mt-6'>
+                            <svg
+                                xmlns='http://www.w3.org/2000/svg'
+                                width='24'
+                                height='24'
+                                viewBox='0 0 24 24'
+                                fill='none'
+                                stroke='currentColor'
+                                strokeWidth='2'
+                                strokeLinecap='round'
+                                strokeLinejoin='round'
+                                className='lucide lucide-quote text-gray-500'
+                            >
+                                <path d='M16 3a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2 1 1 0 0 1 1 1v1a2 2 0 0 1-2 2 1 1 0 0 0-1 1v2a1 1 0 0 0 1 1 6 6 0 0 0 6-6V5a2 2 0 0 0-2-2z' />
+                                <path d='M5 3a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2 1 1 0 0 1 1 1v1a2 2 0 0 1-2 2 1 1 0 0 0-1 1v2a1 1 0 0 0 1 1 6 6 0 0 0 6-6V5a2 2 0 0 0-2-2z' />
+                            </svg>
+                            <em className='text-gray-200 pl-5 absolute top-3 text-xl'>
+                                Terima kasih kepada para pahlawan berjas putih
+                                yang selalu berjuang demi kesehatan kita
+                            </em>
+                        </blockquote>
+
+                        <div className='mt-16 text-xs'>
+                            <Link href='/'>
+                                <HoverBorderGradient
+                                    containerClassName='rounded'
+                                    as='button'
+                                >
+                                    Lihat Semua
+                                </HoverBorderGradient>
+                            </Link>
+                        </div>
                     </div>
                 </div>
             </section>
@@ -55,9 +92,7 @@ export default function Home() {
             {/* Pelayanan */}
             <section>
                 <div className='mb-10 text-center font-medium leading-relaxed'>
-                    <h2 className='text-3xl mb-3'>
-                        Pelayanan Poli Rumah Sakit
-                    </h2>
+                    <h2 className='text-3xl mb-3'>Pelayanan Poli</h2>
                     <p className='max-w-[40rem] mx-auto text-sm text-gray-400 leading-relaxed'>
                         Rumah sakit Ernaldi Bahar dengan tenaga medis yang
                         profesional dan fasilitas yang lengkap, kami siap
