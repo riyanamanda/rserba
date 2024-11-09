7d360e90cdca4c21f5728edc79950cf694e6d166
Riyan Amanda <ryant.n92@gmail.com>
2024-11-09 23:15:54 +0700

    refactor: initial commit 2

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # Ernaldi Bahar Website
 
+A website to provide informations about Ernaldi Bahar Hospital for visitors. ERD (Entity Relationship Diagram) is provided [here](https://app.eraser.io/workspace/7Yf9oV0lyqN9rxqNQllq?origin=share).
 
-https://app.eraser.io/workspace/7Yf9oV0lyqN9rxqNQllq?origin=share
+---
+## Tech Stack
+
+- Next JS
+- Spring Boot
+---
---
+++ server/src/main/resources/db/migration/V1__create_polyclinics_table.sql
@@ -0,0 +1,10 @@
+create table if not exists polyclinics
+(
+    id          int,
+    name        varchar(20) unique not null,
+    slug        varchar(20)        not null,
+    description varchar(100)       not null,
+    is_active   bool                        default true,
+    created_at  timestamp          not null default current_timestamp,
+    updated_at  timestamp
+)
