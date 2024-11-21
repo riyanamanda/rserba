08091e9c2a0b62e4b921df16a9eac31d21492fc8
Riyan Amanda <ryant.n92@gmail.com>
2024-11-21 22:14:51 +0700

    refactor: change ResponseStatusException

--- server/src/main/java/com/erba/server/exception/WebExceptionHandler.java
+++ server/src/main/java/com/erba/server/exception/WebExceptionHandler.java
@@ -1,20 +1,35 @@
 package com.erba.server.exception;
 
 import com.erba.server.response.ValidationError;
+import com.erba.server.response.WebResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
-public class ExceptionHandler {
+public class WebExceptionHandler {
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<WebResponse> apiResponse(ResponseStatusException exception) {
+        return ResponseEntity.status(exception.getStatusCode()).body(
+                WebResponse.builder()
+                        .code(exception.getStatusCode().value())
+                        .status(exception.getBody().getTitle())
+                        .message(exception.getReason())
+                        .build()
+        );
+    }
+
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @org.springframework.web.bind.annotation.ExceptionHandler(MethodArgumentNotValidException.class)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ValidationError> handleValidationErrors(MethodArgumentNotValidException exception) {
         Map<String, String> errors = new HashMap<>();
         exception.getBindingResult().getAllErrors().forEach((error) -> {
