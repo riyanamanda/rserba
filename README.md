727f03753fe1220f406db65f027c654d20bad32d
Riyan Amanda <ryant.n92@gmail.com>
2024-12-25 19:19:27 +0700

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Ernaldi Bahar Hospital
+
+Website to provide informations about Ernaldi Bahar Hospital for visitors. ERD (Entity Relationship Diagram) is provided [here](https://app.eraser.io/workspace/7Yf9oV0lyqN9rxqNQllq?origin=share).
+
+### Tech & Library
+
+- [React]()
+- [Spring Boot]()
+- [Postgre]()
+- [Tailwind]()
---
+++ server/src/main/java/com/ran/erba/ErbaApplication.java
@@ -0,0 +1,13 @@
+package com.ran.erba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErbaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErbaApplication.class, args);
+	}
+
+}
