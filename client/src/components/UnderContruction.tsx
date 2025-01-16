f27fab31e4b25d3786b9ae9867ba732aba9204c7
Riyan Amanda <ryant.n92@gmail.com>
2025-01-17 00:00:34 +0700

    refactor: fix under construction component

---
+++ client/src/components/UnderContruction.tsx
@@ -0,0 +1,11 @@
+const UnderContruction = () => {
+    return (
+        <div className='w-full h-full flex flex-1 items-center justify-center'>
+            <p className='font-medium tracking-wide px-10 py-5 border border-primary uppercase rounded mt-24 shadow-xl shadow-primary/30 bg-primary text-sm animate-pulse'>
+                Under Contruction
+            </p>
+        </div>
+    );
+};
+
+export default UnderContruction;
