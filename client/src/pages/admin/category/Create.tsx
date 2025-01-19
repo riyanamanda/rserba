1fb1ee4147954f3702470f30ebc26bf2876030fe
Riyan Amanda <ryant.n92@gmail.com>
2025-01-19 23:04:01 +0700

    feat: add create category page as under contruction

---
+++ client/src/pages/admin/category/Create.tsx
@@ -0,0 +1,11 @@
+import UnderContruction from '@/components/UnderContruction';
+
+const CreateCategoryPage = () => {
+    return (
+        <div>
+            <UnderContruction />
+        </div>
+    );
+};
+
+export default CreateCategoryPage;
